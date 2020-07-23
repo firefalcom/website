f0ef205163c8a058e389a78f0f9697e145aadfa7
Alayan10 <guillaume.urbain@firefal.com>
2020-07-23 15:26:50 +0200

    Optimization

--- js/functionality.js
+++ js/functionality.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
 
-	var currentAnimation = "before";
+	// Detect browser
+	var isFirefox = typeof InstallTrigger !== 'undefined';
+	var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
+	if(isFirefox == true || isSafari == true){
+		$("[class^='background_section'], #planet, #sun, #city_block").css("transition", "all ease-in-out 0.25s");
+	}
+	else{
+		$("[class^='background_section'], #planet, #sun, #city_block").css("transition", "none");
+	}
+
+	var currentAnimation = "before";
 	// capture scroll any percentage
 	$(window).scroll(function(){
 		let wintop = $(window).scrollTop();
@@ -21,54 +31,55 @@ $(document).ready(function(){
 			}
 		}
 
+		if(isFirefox != true){
+			//Parallax
+			$("[class^='background_section'], #planet, #sun, #city_block").each(function (e) { 
+				let strength = -2;
+				let paralax = wintop / strength; // calculate the paralax
+				let paralax_fault = $(this).offset().top / strength; // calculate the error size
 
-		//Parallax
-		$("[class^='background_section'], #planet, #sun, #city_block").each(function (e) { 
-			let strength = -2;
-			let paralax = wintop / strength; // calculate the paralax
-			let paralax_fault = $(this).offset().top / strength; // calculate the error size
-
-			$(this).css("top", paralax_fault - paralax);
-		});
+				$(this).css("top", paralax_fault - paralax);
+			});
 
 		
-		if($(window).width() >= 1000){	//Lock parallax for small screen
-			// PORTALS
-			$(".hole_content").each(function (e) { 
-				let strength = 3.5;
-				let paralax = wintop / strength; // calculate the paralax
-				let default_position = (80 * ($(window).height() / 100)) - (15 * ($(window).width() / 100));
-				let paralax_fault = ($(this).height() * 1.5 - $(this).offset().top) / strength; // calculate the fault due to the paralax
+			if($(window).width() >= 1000){	//Lock parallax for small screen
+				// PORTALS
+				$(".hole_content").each(function (e) { 
+					let strength = 3.5;
+					let paralax = wintop / strength; // calculate the paralax
+					let default_position = (80 * ($(window).height() / 100)) - (15 * ($(window).width() / 100));
+					let paralax_fault = ($(this).height() * 1.5 - $(this).offset().top) / strength; // calculate the fault due to the paralax
 
-				$(this).css("bottom", default_position - paralax_fault - paralax);
-			});
+					$(this).css("bottom", default_position - paralax_fault - paralax);
+				});
 
-			$(".wave_BG_01").each(function (e) { 
-				let strength = 3;
-				let paralax = wintop / strength; // calculate the paralax
-				let default_position = (80 * ($(window).height() / 100)) - (2 * ($(window).width() / 100));
-				let paralax_fault = ($(this).height() * 1.5 - $(this).offset().top) / strength; // calculate the fault due to the paralax
+				$(".wave_BG_01").each(function (e) { 
+					let strength = 3;
+					let paralax = wintop / strength; // calculate the paralax
+					let default_position = (80 * ($(window).height() / 100)) - (2 * ($(window).width() / 100));
+					let paralax_fault = ($(this).height() * 1.5 - $(this).offset().top) / strength; // calculate the fault due to the paralax
 
-				$(this).css("bottom", default_position - paralax_fault - paralax);
-			});
+					$(this).css("bottom", default_position - paralax_fault - paralax);
+				});
 
-			$(".wave_BG_02").each(function (e) { 
-				let strength = 2;
-				let paralax = wintop / strength; // calculate the paralax
-				let default_position = (80 * ($(window).height() / 100)) + (8 * ($(window).width() / 100));
-				let paralax_fault = ($(this).height() * 2 - $(this).offset().top) / strength; // calculate the fault due to the paralax
+				$(".wave_BG_02").each(function (e) { 
+					let strength = 2;
+					let paralax = wintop / strength; // calculate the paralax
+					let default_position = (80 * ($(window).height() / 100)) + (8 * ($(window).width() / 100));
+					let paralax_fault = ($(this).height() * 2 - $(this).offset().top) / strength; // calculate the fault due to the paralax
 
-				$(this).css("bottom", default_position - paralax_fault - paralax);
-			});
+					$(this).css("bottom", default_position - paralax_fault - paralax);
+				});
 
-			$(".glow").each(function (e) { 
-				let strength = 2;
-				let paralax = wintop / strength; // calculate the paralax
-				let default_position = (80 * ($(window).height() / 100)) - (10 * ($(window).width() / 100));
-				let paralax_fault = ($(this).height() * 0.45 - $(this).offset().top) / strength; // calculate the fault due tot the paralax
+				$(".glow").each(function (e) { 
+					let strength = 2;
+					let paralax = wintop / strength; // calculate the paralax
+					let default_position = (80 * ($(window).height() / 100)) - (10 * ($(window).width() / 100));
+					let paralax_fault = ($(this).height() * 0.45 - $(this).offset().top) / strength; // calculate the fault due tot the paralax
 
-				$(this).css("bottom", default_position - paralax_fault - paralax);
-			});
+					$(this).css("bottom", default_position - paralax_fault - paralax);
+				});
+			}
 		}
 	});
 
